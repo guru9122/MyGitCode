e3422724a6643a0309a3b98ab18431593b85cef9
guru9122 <gurukumariemcal9122@gmail.com>
2021-07-24 20:43:08 +0530

    first commit

---
+++ src/MyFirstGit.java
@@ -0,0 +1,8 @@
+public class MyFirstGit {
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello Guys,This is my first code in GITHUB");
+    }
+
+}
---
+++ src/MySecondGit.java
@@ -0,0 +1,8 @@
+public class MySecondGit {
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello Guys,This is my Second code in GITHUB");
+    }
+
+}
